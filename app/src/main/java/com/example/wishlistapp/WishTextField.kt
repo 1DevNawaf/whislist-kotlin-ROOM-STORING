3390dc9b1abe8398c508b234781f50b98a6b9ed6
NawafAlGhodyah <nwaf557@gmail.com>
2024-04-10 01:22:39 +0300

    Designing a text field

---
+++ app/src/main/java/com/example/wishlistapp/WishTextField.kt
@@ -0,0 +1,34 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun WishTextField(
+    label:String,
+    value: String,
+    onValueChanged: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange =onValueChanged,
+        label ={ Text(text = label, color = Color.Black)},
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = Color.Black,
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Black,
+            cursorColor = Color.Black,
+            focusedLabelColor = Color.Black,
+            unfocusedLabelColor = Color.Black
+        )
+    )
+}
