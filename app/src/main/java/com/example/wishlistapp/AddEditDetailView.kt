a57e536fc676a287fa0fdf725b4f58efc476c7c7
NawafAlGhodyah <nwaf557@gmail.com>
2024-04-10 01:10:51 +0300

    AddEditDetailView

---
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -0,0 +1,42 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun AddEditDetailView(
+    id:Long,
+    viewModel: WishViewModel,
+    navController: NavController
+    ) {
+    Scaffold(
+        topBar = {
+            AppBarView(
+                title = if (id!=0L) "UpdateWish" else "Add Wish"
+            ) {}
+        }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(it)
+                .wrapContentSize()
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+        }
+
+    }
+}
