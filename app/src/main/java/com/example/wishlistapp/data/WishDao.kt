06c0286470cbcfb649433cb3ea4ec18648c3eb4b
NawafAlGhodyah <nwaf557@gmail.com>
2024-04-11 23:21:11 +0300

    Setting the DAO

--- app/src/main/java/com/example/wishlistapp/data/WishDao.kt
+++ app/src/main/java/com/example/wishlistapp/data/WishDao.kt
@@ -1,5 +1,13 @@
 package com.example.wishlistapp.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
 /*
 
 Defining The DAOs :
@@ -16,3 +24,31 @@ Defining The DAOs :
     note that the insert, update and delete are suspend functions
  */
 
+
+
+//Dao must be an abstract class that have abstract fun, because the class it self don't need to perform anything
+@Dao
+abstract class WishDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)//when conflict accrue, just ignore
+    abstract suspend fun addWish(wishEntity: Wish)//best practice to call the object an object's entity
+
+    //define what needs to be fetched
+    @Query("SELECT * FROM `wish-table`")
+    //Flow is a reactive stream, like the suspend, its a part of the Kotlin coroutines
+    //because of that it doesn't need to be suspend hence its already a flow
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
+
+    //edit an entry
+    @Update
+    abstract suspend fun updateWish(wishEntity: Wish)
+
+    //delete an entry
+    @Delete
+    abstract suspend fun deleteWish(wishEntity: Wish)
+
+    //fetch a wish by its id
+    @Query("SELECT * FROM `wish-table` WHERE id=:id")
+    abstract fun getWishById(id:Long): Flow<Wish>
+}
